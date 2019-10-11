a70051c558196cc646cfbe4efa0e80f896425d2a
naimo <naimo@users.noreply.github.com>
2019-10-11 15:00:52 +0200

    add proxy fail/success stats

--- proxy_manager/proxy_manager.py
+++ proxy_manager/proxy_manager.py
@@ -2,6 +2,7 @@
 
 import random
 import requests
+import datetime
 
 class ProxyManager():
     """Holds a list of proxies and handling tools"""
@@ -18,8 +19,11 @@ class ProxyManager():
     def get_random_proxy(self):
         return random.choice(self.proxies)
 
-    def remove_bad_proxy(self, proxy):
+    def remove_proxy(self, proxy):
         self.proxies.remove(proxy)
+
+    def add_proxy(self, proxy):
+        self.proxies.append(proxy)
 
     def to_file(self, filename):
         with open(filename, 'w') as export_file:
@@ -30,6 +34,10 @@ class Proxy():
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.successes = 0
+        self.fails = 0
+        self.consecutive_fails = 0
+        self.bans = [None]
 
     def __eq__(self, other):
         if isinstance(other, Proxy):
@@ -41,6 +49,40 @@ class Proxy():
 
     def get_url(self):
         return self.host+":"+str(self.port)
+
+    def ban(self):
+        self.bans[-1] = datetime.datetime.now()
+        return
+
+    def unban(self):
+        self.bans.append(None)
+        return
+
+    def is_banned(self):
+        return self.bans[-1] is not None
+
+    def fail(self):
+        self.fails += 1
+        self.consecutive_fails += 1
+        return
+
+    def succeed(self):
+        self.successes += 1
+        self.consecutive_fails = 0
+        return
+
+    def stats(self):
+        total = self.successes+self.fails
+        if total>0:
+            fail_ratio = self.fails/(self.successes+self.fails)
+        else:
+            fail_ratio = 0
+        return fail_ratio,self.consecutive_fails
+
+    def last_ban_hours(self):
+        if self.is_banned():
+            ban_hours = (datetime.datetime.now() - self.bans[-1]).seconds/3600
+            return ban_hours
 
     def test_proxy(self):
         proxy_url = self.get_url()
@@ -55,6 +97,25 @@ if __name__ == "__main__":
     # proxies = ["108.61.186.207:8080","118.27.31.50:3128","5.196.132.117:3128"]
     # proxymanager = ProxyManager(proxies)
     proxymanager = ProxyManager.from_file("proxies")
+
     random_proxy = proxymanager.get_random_proxy()
+
     print(random_proxy, random_proxy.test_proxy())
+    print(random_proxy.is_banned())
+    random_proxy.ban()
+    print(random_proxy.is_banned(), random_proxy.last_ban_hours())
+    random_proxy.unban()
+    print(random_proxy.is_banned(), random_proxy.bans)
+
+    print(random_proxy.stats())
+    random_proxy.succeed()
+    print(random_proxy.stats())
+    random_proxy.fail()
+    random_proxy.fail()
+    print(random_proxy.stats())
+    random_proxy.succeed()
+    print(random_proxy.stats())
+    random_proxy.fail()
+    print(random_proxy.stats())
+
     proxymanager.to_file('test')
